0608d4bc1470cfc40abcae7e579c82733492f2be
WinelandThomson <it@thomsonsafaris.com>
2018-10-16 19:03:50 +0000

    added plugin file

---
+++ ts-cross-domain-cookie.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package TSCrossDomainCookie
+ */
+/*
+Plugin Name: TS Cross Domain Cookie
+Plugin URI: https://thomson-safaris/CrossDomainCookies.git
+Description: Remember google/bing PPC across sub-domains
+Version: 1.0.0
+Author: Jim Chapman - Thomson Safaris
+License: GPLv2 or later
+Text Domain: tscrossdomaincookie
+*/
+$settings = htmlentities(json_encode($_GET));
+?>
+<img src="https://basecamp.thomsonsafaris.com/basecamp/api/v1/cdcookie/<?= $settings ?>" style="display:none;" />
