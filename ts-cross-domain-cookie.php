d008fa1a8eb0f03822683afeae1b336316809788
WinelandThomson <it@thomsonsafaris.com>
2018-11-08 17:19:32 +0000

    another test

--- ts-cross-domain-cookie.php
+++ ts-cross-domain-cookie.php
@@ -6,19 +6,14 @@
 Plugin Name: TS Cross Domain Cookie
 Plugin URI: https://github.com/thomson-safaris/CrossDomainCookies.git
 Description: Remember google/bing PPC across sub-domains
-Version: 1.0.2
+Version: 2.0.0
 GitHub Plugin URI: thomson-safaris/CrossDomainCookies
 Author: Jim Chapman - Thomson Safaris
 License: GPLv2 or later
 Text Domain: tscrossdomaincookie
 */
-$settings = htmlentities(json_encode($_GET));
+
+$test = 'nothing really happens yet'
+
+
 ?>
-<div id="crossDomainCookie" style="display:none;">
-    <img src="https://basecamp.thomsonsafaris.com/basecamp/api/v1/cdcookie/<?= $settings ?>" style="display:none; height:0px !important;"/>
-</div>
-<script>
-$(document).ready(function(){
-    $('#crossDomainCookie').hide();
-})
-</script>
