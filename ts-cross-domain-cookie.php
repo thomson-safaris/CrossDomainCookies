ee8b22c9803b736203f8145f6a96f1c3e343dc99
WinelandThomson <it@thomsonsafaris.com>
2018-11-08 17:17:39 +0000

    added auto update from git

--- ts-cross-domain-cookie.php
+++ ts-cross-domain-cookie.php
@@ -7,6 +7,7 @@ Plugin Name: TS Cross Domain Cookie
 Plugin URI: https://github.com/thomson-safaris/CrossDomainCookies.git
 Description: Remember google/bing PPC across sub-domains
 Version: 1.0.2
+GitHub Plugin URI: thomson-safaris/CrossDomainCookies
 Author: Jim Chapman - Thomson Safaris
 License: GPLv2 or later
 Text Domain: tscrossdomaincookie
